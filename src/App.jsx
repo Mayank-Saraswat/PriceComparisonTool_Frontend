c7fb2833d5e029d05a27f66e49eebdc417aa8a03
Mayank-Saraswat <mayanksaraswat497@gmail.com>
2024-04-30 20:32:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [product, setProduct] = useState("");
+  const [prices, setPrices] = useState({});
+  const [minPrice, setMinPrice] = useState(Infinity);
+
+  const fetchPrices = async () => {
+    try {
+      if (!product) {
+        alert("Please enter a valid product name");
+        return;
+      }
+      const response = await axios.get(
+        `http://localhost:5000/api/prices?product=${product}`
+      );
+
+      const cleanedPrices = {
+        Amazon: parseFloat(
+          response.data.Amazon.replace("₹", "").replace(",", "")
+        ),
+        Flipkart: parseFloat(
+          response.data.Flipkart.replace("₹", "").replace(",", "")
+        ),
+      };
+      setPrices(cleanedPrices);
+      setMinPrice(Math.min(cleanedPrices.Amazon, cleanedPrices.Flipkart));
+    } catch (error) {
+      console.error("Error fetching prices:", error);
+    }
+  };
+
+  const getBestPrice = () => {
+    const { Amazon, Flipkart } = prices;
+    let bestPricePlatform = "";
+    if (minPrice === Amazon && minPrice === Flipkart) {
+      bestPricePlatform = "both Amazon and Flipkart";
+    } else if (minPrice === Amazon && minPrice !== Flipkart) {
+      bestPricePlatform = "Amazon";
+    } else if (minPrice === Flipkart && minPrice !== Amazon) {
+      bestPricePlatform = "Flipkart";
+    }
+    return { bestPricePlatform };
+  };
+
+  const { bestPricePlatform } = getBestPrice();
+
+  return (
+    <>
+      <h2 className="m-3 text-center text-5xl font-semibold text-sky-800">
+        Price Comparison Tool
+      </h2>
+      <h2 className="mx-auto text-center text-lg md:w-1/2">
+        Track your competition in real-time, compare prices and make better
+        pricing decisions. The main purpose of this price comparison tool is to
+        help you monitor competitors’ prices in order to adjust your pricing
+        strategy accordingly.
+      </h2>
+      <div className="m-5 text-center">
+        <input
+          type="text"
+          value={product}
+          onChange={(e) => setProduct(e.target.value)}
+          placeholder="Enter product name"
+          className="p-1 mr-2 mb-2 w-full md:w-1/3 border-2 border-green-600"
+        />
+        <button
+          className="py-1 px-5 bg-green-600 rounded text-white font-semibold hover:bg-green-700"
+          onClick={fetchPrices}
+        >
+          Get Prices
+        </button>
+
+        {minPrice !== Infinity && (
+          <div>
+            <div className="mt-5">
+              <h2>Prices comparison result (in rupees)</h2>
+              <ul className="text-xl">
+                {prices.Amazon && <li>Amazon: ₹{prices.Amazon}</li>}
+                {prices.Flipkart && <li>Flipkart: ₹{prices.Flipkart}</li>}
+              </ul>
+            </div>
+
+            <div className="mt-2 p-2 text-2xl text-rose-600 w-full md:w-1/2 mx-auto shadow-2xl">
+              {bestPricePlatform === "both Amazon and Flipkart" ? (
+                <span>
+                  Best price after comparison is the same on both Amazon and
+                  Flipkart: ₹{minPrice}. <br />
+                  Please consider checking other factors before making a
+                  decision. Thanks for using our tool.
+                </span>
+              ) : (
+                <span>
+                  Best price after comparison: ₹{minPrice}. <br />
+                  So, We recommend {bestPricePlatform} for your purchase. Thanks
+                  for using our tool.
+                </span>
+              )}
+            </div>
+          </div>
+        )}
+        <div className="mt-3 mx-auto w-full md:w-1/2">
+          Note: Please press on Get Prices button more than once if you are not
+          getting a price as sometimes data fetching takes some time.
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
